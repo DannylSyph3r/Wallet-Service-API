fef372c25e94f089732a0f7cd37891ca0b0211ab
DannylSyph3r <dopedanny001@gmail.com>
2025-12-09 16:42:32 +0100

    Chore: Initialize Project

---
+++ src/test/java/dev/slethware/walletservice/WalletserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.slethware.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
