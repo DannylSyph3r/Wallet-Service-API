12ae3b1c3b69ca1c931ef7d8bc6db3eefb253fbd
DannylSyph3r <dopedanny001@gmail.com>
2025-12-10 14:14:52 +0100

    Fix: Cors for Railway deployment

--- src/main/java/dev/slethware/walletservice/config/CorsConfig.java
+++ src/main/java/dev/slethware/walletservice/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package dev.slethware.walletservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,20 +13,24 @@ import java.util.Collections;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins:*}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
         configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Disposition", "x-api-key"));
+        configuration.setExposedHeaders(Arrays.asList(
+                "Authorization",
+                "Content-Disposition",
+                "x-api-key"
+        ));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
         return source;
     }
 }
